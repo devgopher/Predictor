c6fdddacf582006009839ca9972009f6a30a598f
Igor Evdokimov <ievdokimov.csharp@gmail.com>
2026-09-03 16:45:20 +0300

    - encreased limit for url fetcher

--- src/Predictor.Sample/Program.cs
+++ src/Predictor.Sample/Program.cs
@@ -27,7 +27,7 @@ Console.WriteLine();
 
 const string facts =
     """
-    https://ru.wikipedia.org/wiki/%D0%92%D1%8B%D0%B1%D0%BE%D1%80%D1%8B_%D0%B2_%D0%9F%D0%B0%D0%BB%D0%B0%D1%82%D1%83_%D0%BF%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B0%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D0%B5%D0%B9_%D0%A1%D0%A8%D0%90_(2026)
+    https://en.wikipedia.org/wiki/2026_United_States_House_of_Representatives_elections
     """;
 
 Console.WriteLine("=== 1. Embedding ===");
